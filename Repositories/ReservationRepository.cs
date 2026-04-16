760b20b824628f93f91e9356139bb2e9f8e44968
fernandobmx <fernando.martinez.mora@est.una.ac.cr>
2026-04-16 06:02:02 -0600

    feat: implement  method to delete a reservation by id on repository

--- Repositories/ReservationRepository.cs
+++ Repositories/ReservationRepository.cs
@@ -6,34 +6,47 @@ namespace reservations_api.Repositories;
 
 public class ReservationRepository : IReservationRepository
 {
-  private readonly AppDbContext _context;
+    private readonly AppDbContext _context;
 
-  public ReservationRepository(AppDbContext context)
-  {
-    _context = context;
-  }
+    public ReservationRepository(AppDbContext context)
+    {
+        _context = context;
+    }
 
-  public async Task<Reservation> AddAsync(Reservation reservation)
-  {
-    await _context.Reservations.AddAsync(reservation);
-    await _context.SaveChangesAsync();
-    return reservation;
-  }
+    public async Task<Reservation> AddAsync(Reservation reservation)
+    {
+        await _context.Reservations.AddAsync(reservation);
+        await _context.SaveChangesAsync();
+        return reservation;
+    }
 
-  public async Task<List<Reservation>> GetByClassroomAndDateAsync(Guid classroomId, DateOnly date)
-  {
-    return await _context.Reservations
-        .AsNoTracking()
-        .Where(r => r.ClassroomId == classroomId && r.Date == date)
-        .ToListAsync();
-  }
+    public async Task<List<Reservation>> GetByClassroomAndDateAsync(Guid classroomId, DateOnly date)
+    {
+        return await _context.Reservations
+            .AsNoTracking()
+            .Where(r => r.ClassroomId == classroomId && r.Date == date)
+            .ToListAsync();
+    }
 
-  public async Task<List<Reservation>> GetAllReservationsByDateAsync(DateOnly date)
-  {
-    return await _context.Reservations
-        .AsNoTracking()
-        .Where(r => r.Date == date)
-        .OrderBy(r => r.StartTime)
-        .ToListAsync();
-  }
+    public async Task<List<Reservation>> GetAllReservationsByDateAsync(DateOnly date)
+    {
+        return await _context.Reservations
+            .AsNoTracking()
+            .Where(r => r.Date == date)
+            .OrderBy(r => r.StartTime)
+            .ToListAsync();
+    }
+
+    public async Task<bool> DeleteReservationByIdAsync(Guid id)
+    {
+        var reservation = await _context.Reservations.FindAsync(id);
+        if (reservation == null)
+        {
+            return false;
+        }
+
+        _context.Reservations.Remove(reservation);
+        await _context.SaveChangesAsync();
+        return true;
+    }
 }
