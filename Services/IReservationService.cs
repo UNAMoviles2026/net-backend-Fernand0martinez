4e73d73dfcf7e35931528c1dbbe631a6922882ea
fernandobmx <fernando.martinez.mora@est.una.ac.cr>
2026-04-16 06:32:59 -0600

    feat: remove comment on services no needed

--- Services/IReservationService.cs
+++ Services/IReservationService.cs
@@ -6,9 +6,6 @@ namespace reservations_api.Services;
 public interface IReservationService
 {
     Task<ReservationResponse> CreateAsync(CreateReservationRequest request);
-
-    // This method is added to get all reservations for a specific date, regardless of the classroom
     Task<List<ReservationResponse>> GetAllReservationsByDateAsync(DateOnly date);
-
     Task<bool> DeleteReservationByIdAsync(Guid id);
 }
